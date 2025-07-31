7cfc342b8450ff842a3584d6cad0ec6e76fdb612
DevMehdi84 <mahdi.safaeinia84@gmail.com>
2025-08-01 01:46:21 +0330

    Complete navigation drawer UI (pending click handlers)

--- app/src/main/java/com/example/wikipedia/MainActivity.kt
+++ app/src/main/java/com/example/wikipedia/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.wikipedia
 
-import android.graphics.Color
 import android.os.Bundle
-import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.wikipedia.databinding.ActivityMainBinding
@@ -17,5 +16,47 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbarmain)
         window.statusBarColor = ContextCompat.getColor(this, R.color.md_theme_primary)
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.dravermain,
+            binding.toolbarmain,
+            R.string.open_Drawer,
+            R.string.close_Drawer
+        )
+        binding.dravermain.addDrawerListener( actionBarDrawerToggle )
+        actionBarDrawerToggle.syncState()
+
+        binding.navmain.setNavigationItemSelectedListener {
+            when(it.itemId){
+
+                R.id.menu_writer ->{
+
+                }
+
+                R.id.menu_Translator ->{
+
+
+                }
+                R.id.menu_photographer ->{
+
+                }
+
+                R.id.menu_video_maker ->{
+
+                }
+
+                R.id.menu_wikimedia ->{
+
+                }
+                R.id.openWikipedia ->{
+
+                }
+
+
+            }
+            true
+        }
+
     }
 }
