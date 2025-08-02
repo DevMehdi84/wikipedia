2e8f27197f7cf96fe3124bbe0dd240bf36420b7c
DevMehdi84 <mahdi.safaeinia84@gmail.com>
2025-08-02 20:30:39 +0330

    design and complete XML layout for MainActivity2 screen

--- app/src/main/java/com/example/wikipedia/MainActivity2.kt
+++ app/src/main/java/com/example/wikipedia/MainActivity2.kt
@@ -1,8 +1,10 @@
 package com.example.wikipedia
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.wikipedia.databinding.ActivityMain2Binding
@@ -14,6 +16,27 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
+        setSupportActionBar(binding.toolabarmain2)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.md_theme_primary)
 
+        binding.collpsingMain.setExpandedTitleColor(
+            ContextCompat.getColor(this , android.R.color.transparent)
+        )
+        supportActionBar!!.setHomeButtonEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        //set a color back botton
+        binding.toolabarmain2.navigationIcon?.setTint(ContextCompat.getColor(this, R.color.md_theme_surfaceContainerLow))
+
+
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == android.R.id.home){
+            onBackPressed()
+        }
+
+      return true
     }
 }
